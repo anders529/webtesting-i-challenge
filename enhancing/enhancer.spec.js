3a6f2603f342e6f5ac3070f39c5cbbef02b78103
anders529 <anderseana@tutanota.com>
2020-03-26 11:21:25 -0500

    Cleaned up the enhancer.js file. Half way done with MVP.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,26 @@
 const enhancer = require('./enhancer.js');
 // test away!
+it('repair', () => {
+    expect(enhancer.repair({
+        name: 'Aaron',
+        durability: 95,
+        enhancement: 30
+    }))
+        .toEqual({
+            name: 'Aaron',
+            durability: 100,
+            enhancement: 30
+        });
+})
+it('succeed', () => {
+    expect(enhancer.succeed({
+        name: 'Aaron',
+        durability: 80,
+        enhancement: 20
+    }))
+        .toEqual({
+            name: 'Aaron',
+            durability: 80,
+            enhancement: 25
+        })
+})
